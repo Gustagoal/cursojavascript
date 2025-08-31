a29cc8132d964a4b19e6e64950d94306654cc52e
unknown <gustavocastruu@gmail.com>
2025-08-31 13:33:19 -0300

    Aprendendo if e else

---
+++ 08/index.js
@@ -0,0 +1,9 @@
+// condições
+console.log("Ola , testando o JavaScript")
+var idade = 18
+if (idade >=18){
+    console.log(`Sua idade é ${idade} esta autorizado a entrar na balada`)
+}
+else {
+    console.log(`Sua idade é ${idade} não pode ir para a balada`)
+} 
