aa342c9b193a2624f8e93753d9f224e94cef836e
unknown <gustavocastruu@gmail.com>
2025-11-01 15:25:28 -0300

    Aprendeno funções JS"

---
+++ funcoes.js
@@ -0,0 +1,19 @@
+//  Funções JS
+
+function mensagem(){
+    console.log("Teste de função")
+}
+function Mostrar_nome(nome){
+    console.log("Seu nome é " + nome)
+}
+function Maior_idade(idade){
+    if (idade>=18){
+        console.log("Maior de idade")
+    }
+    else{
+        console.log("Menor de idade")
+    }
+}
+ function calcularMedia(nota1, nota2) { const media = (nota1 + nota2) / 2; } console.log(calcularMedia(5, 5))
+
+ calcularMedia(5,5)
